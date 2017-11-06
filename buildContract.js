75bffccda32f490337620afd8a45a6d2ce12129d
Evan Botello <botellde@gmail.com>
2017-11-06 18:37:57 -0500

    updates

--- buildContract.js
+++ buildContract.js
@@ -3,20 +3,21 @@ const solc = require('solc');
 const Web3 = require('web3');
 
 const web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
-let source = fs.readFileSync('contract.sol', 'utf8');
-let compiledContract = solc.compile(source, 1);
-let abi = compiledContract.contracts[':greeter'].interface;
-let hexBytecode = web3.toHex(compiledContract.contracts[':greeter'].bytecode);
-let gasEstimate = web3.eth.estimateGas({data: hexBytecode});
-let greeterContract = web3.eth.contract(JSON.parse(abi));
-const greeting = "Hello World!"
-const options = {from:web3.eth.accounts[0], data: hexBytecode, gas: gasEstimate};
+const source = fs.readFileSync('contract.sol', 'utf8');
+const compiledContract = solc.compile(source, 1).contracts[':greeter'];
+const abi = compiledContract.interface;
 
-greeterContract.new(greeting, options, function(e, contract){
+// append 0x so geth can parse
+const bytecode = `0x${compiledContract.bytecode}`;
+const gasEstimate = web3.eth.estimateGas({data: bytecode});
+const greeterContract = web3.eth.contract(JSON.parse(abi));
+const greeting = 'Hello World!';
+const options = {from: web3.eth.accounts[0], data: bytecode, gas: (gasEstimate*2)};
+
+greeterContract.new(greeting, options, (e, contract) => {
   if(!e) {
     if(!contract.address) {
       console.log("Contract transaction send: TransactionHash: " + contract.transactionHash + " waiting to be mined...");
-
     } else {
       console.log("Contract mined! Yay! Address: " + contract.address);
       console.log(contract);
